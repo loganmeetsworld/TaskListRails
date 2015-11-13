de282f92012eee827cb6dffc85e93de80a4332db
Logan McDonald <loganmcdona11@gmail.com>
2015-11-13 09:10:30 -0800

    Wave three complete

--- tasklist/app/controllers/tasks_controller.rb
+++ tasklist/app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
 	def index
 		@tasks = Task.all
+		@tasks = @tasks.order(:complete_date, :name)
 	end
 
 	def show
@@ -17,13 +18,40 @@ class TasksController < ApplicationController
 		redirect_to "/"
 	end
 
-	# def edit
-	# 	@task = Task.find(params[:id])
-	# 	render :new
-	# end
-
 	def create
 		Task.create(task_params[:task])
+		redirect_to "/"
+	end
+
+	def edit 
+		show
+	end
+
+	def update
+		show
+
+		@task.update(name: params[:task][:name],
+								description: params[:task][:description],
+								complete_date: params[:task][:complete_date])
+		@task.save
+
+		redirect_to "/"
+	end
+
+	def complete
+		show
+
+		@task.complete_date = Time.now
+		@task.save
+
+		redirect_to "/"
+	end
+
+	def incomplete
+		show
+		@task.complete_date = nil
+		@task.save
+
 		redirect_to "/"
 	end
 
--- tasklist/config/routes.rb
+++ tasklist/config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
 	root 'tasks#index'
-	get '/tasks/new' => 'tasks#new'
-	post '/tasks' => 'tasks#create'
+	get 'tasks/new' => 'tasks#new' # Method and the data
+	post 'tasks' => 'tasks#create' # Just the action
 
-	get '/tasks/:id' => 'tasks#show', as: :task
-	delete '/tasks/:id' => 'tasks#delete'
+	get 'tasks/:id' => 'tasks#show', as: :task
+	delete 'tasks/:id' => 'tasks#delete'
 
 	# Edit
-	# get '/update/:id' => 'tasks#edit'
+	get 'tasks/update/:id' => 'tasks#edit'
+	post 'tasks/:id/edit' => 'tasks#update'
+	patch 'tasks/:id' => 'tasks#update'
 
-	# Testing out my 404s, lowest priority
-	# get '/:anything' => 'tasks/missing'
-	# get '/:anything/:anything' => 'tasks/missing'
+
+	# Update to complete
+	get 'tasks/complete/:id' => 'tasks#complete'
+	patch 'tasks/complete/:id' => 'tasks#complete'
+
+	# Update to incomplete
+	get 'tasks/incomplete/:id' => 'tasks#incomplete'
+	patch 'tasks/incomplete/:id' => 'tasks#incomplete'
 end
